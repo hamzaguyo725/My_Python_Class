b6ac4ee725a7a1820df575d00001dc77248eb71a
hamzaguyo725 <hamzaguyo725@gmail.com>
2026-09-02 12:26:26 +0300

    first commit

---
+++ hello_world.py
@@ -0,0 +1,32 @@
+# print("from hello world to changing the world")
+
+def double(value):
+    return value * value
+
+value = 5
+result = double(value)
+print(result)
+
+def divide(a,b):
+    return a / b
+
+a = 6
+b = 3
+result = divide(a,b)
+print(result)
+
+def add(value):
+    return value + value
+
+value = 9
+result = add(value)
+print(result)
+
+
+def subtraction(a,b):
+    return a - b
+
+a = 12
+b = 4
+result = subtraction(a,b)
+print(result)
